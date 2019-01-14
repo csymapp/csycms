58008a0c874ac9a49f801c8d9ec4f7ccb2b7bc05
Brian Onang'o <surgbc@gmail.com>
2019-01-14 20:41:32 +0300

    site service

--- csycms.update.sh
+++ csycms.update.sh
@@ -8,14 +8,41 @@ checktime=$((checktime * 60))
 GIT=$(git log|head)
 GITOLD="$(echo $GIT | cut -d' ' -f2)"
 
-while :	
-do
-   {
-   		sleep $checktime
-   		git pull origin master
-        GIT=$(git log|head)
-        GITNEW="$(echo $GIT | cut -d' ' -f2)"
+readConfig () {
+    set -a
+    source ../.env
+    set +a
+}
 
+readConfig
+
+
+mkdir -p content
+cd content
+git clone $SITEREPO .
+
+GITSITE=$(git log|head)
+GITOLDSITE="$(echo $GITSITE | cut -d' ' -f2)"
+
+cd ..
+
+while :
+do
+    {
+        sleep $checktime
+        git pull origin master
+        GIT=$(git log|head)
+        
+        cd content
+        git pull origin master
+        GITSITE=$(git log|head)
+
+        GITOLDSITE="$(echo $GITSITE | cut -d' ' -f2)" 
+        GITNEWSITE="$(echo $GIT | cut -d' ' -f2)"
+        
         [ "$GITOLD" == "$GITNEW" ] || systemctl restart csycms.service
-   }
+        [ "$GITOLDSITE" == "$GITNEWSITE" ] || systemctl restart csycms.service
+        
+        mkdir -p
+    }
 done
