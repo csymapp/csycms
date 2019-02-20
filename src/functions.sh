978c66f4e10b08f5faa4a6cec00706eaceb921f5
Brian Onang'o <surgbc@gmail.com>
2019-02-20 16:43:19 +0300

    fixed cp error

--- src/functions.sh
+++ src/functions.sh
@@ -101,9 +101,9 @@ setUpSite () {
     cd ../..
     {
         # cp -r "content/$1/public/content" "content/$1"
-        cp -r "content/$1/public/config/*" "config/$1"
-        cp -r "content/$1/public/public/*" "public/sites/$1"
-        cp -r "content/$1/public/layouts/*" "layouts/$1"
+        cp -r "content/$1/public/config/"* "config/$1"
+        cp -r "content/$1/public/public/"* "public/sites/$1"
+        cp -r "content/$1/public/layouts/"* "layouts/$1"
     } || echo -n""
     
     cd "content/$1"
