9c8ec517f21629c70dfb2dc3c16272faf3478978
Brian Onang'o <surgbc@gmail.com>
2019-11-20 18:31:47 +0300

    error and other pages

--- src/functions.sh
+++ src/functions.sh
@@ -126,7 +126,7 @@ setUpSite () {
         cp -r "content/$1/public/layouts/"* "layouts/$1"
         rm -rf "content/$1/errorpages"
         mkdir -p "content/$1/errorpages"
-        cp -r errorpages "content/$1/errorpages"
+        cp -r pages/* "content/$1/errorpages"
     } || echo -n""
     
     cd "content/$1"
